0a976c9d76014bd1c242348c35d11b3f1317f241
Dominik-Hillmann <dominik.hillmann@gmx.de>
2019-12-29 19:48:35 +0100

    Download of first image done

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # artstation-crawler
 A crawler retrieving pictures from artstation given their tags.
+
+## Usage
+
+## Dependencies
+* Python 2.7
+* `selenium`
+    * Downloaded via, the `geckodriver.exe` has to be put into source directory of Python
+* `splinter` library
+* `bs4` library
--- main.py
+++ main.py
@@ -3,38 +3,58 @@
 
 # Python libraries
 import requests
+import os
 # External libraries
 from bs4 import BeautifulSoup
 # Typing
 from typing import Callable
+import chardet
+from pprint import pprint
+from splinter import Browser
 
-EXAMPLE_URL = 'https://www.artstation.com/artwork/ybG0yx'
+PIC_URL = 'https://www.artstation.com/artwork/ybG0yx'
 
-def main() -> None:
-    header = {
-        'Host': 'www.artstation.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0',
-        'Accept': 'text/css,*/*;q=0.1',
-        'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'DNT': 1,
-        'Connection': 'keep-alive',
-        'Cookie': '__cfduid=d06324c170df1bf0e7f53424c2af5cb9c1577215010; visitor-uuid=1181f858-0bfb-4f82-badd-8b52422f2431; PRIVATE-CSRF-TOKEN=ObRRew0EFHXHtXGcctdj62CuXeHOUaDaX5xXstW4tt4%3D; country_code=DE; continent_code=EU; _pk_testcookie.2.119b=1; _pk_id.2.119b=ccd824f3b7206272.1577215030.3.1577224989.1577224989.; __stripe_mid=6d85f0a9-74dd-45f3-9030-0880f50db8bc; _ArtStation_session=V2x1bWZ4T2ZsaVhGYm1RVHdERWhoMGRTaHh0NVRRZnVpUXdpeFNYZkRmQjRPdUc2NzBlZkRicTNuVXNDcEExd1o0UGFMejA0WW1VUE42UVdSQlZNRkc1aW1lU2J6YjQ2SXg3TTNJcEYrcjlseEkwVThPdk04eWxCNmNXRDUzYlVHSkdzSUI2Z3NIdEdjWktiTDI4dEtENlNKK1pyb09Ua0NsdWNJR3lYNjdXQjZkOGxSblUyclRUdDByb0ptdms0MDhZc0UwU3E2SU1HdWJHVXdKVnZOUT09LS1jUXdNczBWOHlZeUwvOG1JT2ZlaElBPT0%3D--482379c2cfe52d8a7d82e5f091d39b79ca836176; __stripe_sid=96e78c67-329a-4d6c-b603-68f8abcd78cf'
-    }
-    markup = requests.get('http://www.artstation.com/search?q=space ship&sort_by=likes', headers = header)
-    print(type(markup.text))
-    print(markup.status_code)
-    soup = BeautifulSoup(markup.text, features = 'html.parser')
+def main():
+    browser = Browser()
+    browser.visit(PIC_URL)
+    print(browser.title)
+    markup = browser.html.encode('utf-8')
+    soup = BeautifulSoup(markup, 'html.parser')
 
-    print(soup.prettify())
+    pic_urls = []
+    pics = soup.find_all('img', { 'class': 'img' })
+
+    for pic in pics:
+        pic_urls.append(pic['src'].encode('utf-8'))
+
+    print(pic_urls)
+
+    for pic_url in pic_urls:
+        browser.visit(pic_url)
+        browser.screenshot(os.path.join('D:', 'Repositories', 'artstation-crawler', 'test.jpg'))
+
+    browser.quit()
 
 
 def parse_parameters():
     pass
 
 
-def parse_web_pages(start_page_url: str, stop_condition: Callable):
+def get_search_page_links(start_page_url, stop_condition):
+    """[summary]
+    
+    Arguments:
+        start_page_url {[type]} -- [description]
+        stop_condition {[type]} -- [description]
+    """
     pass
+
+
+def add_to_queue():
+    pass
+
+
+
 
 
 def print_stats():
@@ -43,3 +63,4 @@ def print_stats():
 
 if __name__ == '__main__':
     main()
+
